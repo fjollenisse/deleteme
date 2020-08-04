14a7c3dd59c1739e6594d5c1e212790483348b98
fjollenisse <68201313+fjollenisse@users.noreply.github.com>
2020-08-04 08:50:07 +0200

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FjolleCorp
+{
+    public class Nisse
+    {
+        public void Jump(int height, int numberOfTimes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
