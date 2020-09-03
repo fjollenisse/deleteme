96d04b1e7a394b8f33552f59e948c138d4d46448
fjollenisse <68201313+fjollenisse@users.noreply.github.com>
2020-09-03 15:07:33 +0200

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -1,39 +1,18 @@
 public class Car
 {
-    public void Drive(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n)
+    public void Drive(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o)
     {
         throw new System.NotImplementedException();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         // test
     }
     
-    public void Stop(int a, int b, int c, int d, int e, int f, int g, int h, int i)
+    public void Break(int a, int b, int c, int d, int e, int f, int g, int h, int i)
     {
         throw new System.NotImplementedException()
     }
     
-    public void RollOver(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m)
+    public void Accellerate(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m)
     {
         throw new System.NotImplementedException()
     }
