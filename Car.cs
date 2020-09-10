e5dd6c314606aebd11dd6beb6070acb0c7fba62d
fjollenisse <68201313+fjollenisse@users.noreply.github.com>
2020-09-10 10:45:53 +0200

    Commit 1

--- Car.cs
+++ Car.cs
@@ -1,32 +1,20 @@
 public class Car
 {
-    public void Drive(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n)
+    public void Drive(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o)
     {
         throw new System.NotImplementedException();
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        // test
+    }
+    
+    public void Accellerate(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n)
+    {
+        throw new System.NotImplementedException();
         
         // test
     }
+    
+    
     
     public void Stop(int a, int b, int c, int d, int e, int f, int g, int h, int i)
     {
